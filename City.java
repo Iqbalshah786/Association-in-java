c09386da18dae5ec58df50a7c2cebd10d46c51f1
Iqbal Shah Nadiri <81562644+Iqbalshah786@users.noreply.github.com>
2022-05-23 13:05:34 +0500

    Add files via upload

---
+++ City.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class City {
+    private  String name;
+
+
+    public City(String name){
+        this.name = name;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "City : "+getName();
+    }
+
+
+}
